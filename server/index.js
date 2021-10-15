8c10d2937fd8d0a04ec240c9018a476558a049bf
acer <cbhatia2002@gmail.com>
2021-10-15 21:35:45 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+const app = express();
+
+dotenv.config({ path: './config.env' })
+app.use(cors());
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log(`Database connected successfully`);
+})
+.catch((err) => {
+    console.log(`Failed to connect database`);
+});
+
+const PORT = process.env.PORT
+//Listening to port
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`)
+})
