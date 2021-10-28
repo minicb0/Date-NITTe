e90dea14a43db92fc8d63ac2a891102ae6fb3ca7
acer <cbhatia2002@gmail.com>
2021-10-28 23:04:14 +0530

    feat: add eslint to client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Register } from './pages/auth/register'
-import { Login } from './pages/auth/login'
-const App = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-            </Switch>
-        </Router>
-    )
-}
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Register } from "./pages/auth/register";
+import { Login } from "./pages/auth/login";
+
+const App = () => (
+	<Router>
+		<Switch>
+			<Route exact path="/register" component={Register} />
+			<Route exact path="/login" component={Login} />
+		</Switch>
+	</Router>
+);
 
 export default App;
