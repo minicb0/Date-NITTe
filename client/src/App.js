1c2ad12a5a0c205b940846440d048733e87aa4a2
Sanjay R Krishna <krishnakrrish736@gmail.com>
2021-10-29 13:14:40 +0530

    fix[login_page]: Redirecting Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+	BrowserRouter as Router, Route, Switch, Redirect,
+} from "react-router-dom";
 import { Register } from "./pages/auth/register";
 import { Login } from "./pages/auth/login";
 
 const App = () => (
 	<Router>
+		<Route exact path="/">
+			<Redirect to="/login" />
+		</Route>
 		<Switch>
 			<Route exact path="/register" component={Register} />
 			<Route exact path="/login" component={Login} />
